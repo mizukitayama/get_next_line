821e2da8702a17b0b0e93266f5e86cae6f4db8af
mizukitayama <tayama_mizuki@eagle.sophia.ac.jp>
2024-05-25 15:13:12 +0900

    bonus

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtayama <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/19 18:51:47 by mtayama           #+#    #+#             */
+/*   Updated: 2023/03/19 18:55:32 by mtayama          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#	ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(char *str);
+char	*ft_strchr(char *s, int c);
+char	*combine_strs(char *save, char *buf);
+char	*get_next_line(int fd);
+
+#endif
